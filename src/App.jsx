63bc0fac9f88262d52145d1cfe60f1a3d4c9d50f
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-08-31 09:02:59 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Plans from "./pages/Plans";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Register from "./pages/Register";
+import { Navigate } from "react-router-dom";
 
-export default function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/plans" element={<Plans />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/register" element={<Register />} />
-  <Route path="/dashboard" element={<Dashboard />} />
-</Routes>
-      <Footer />
-    </Router>
-  );
+export default function ProtectedRoute({ children }) {
+  const user = localStorage.getItem("loggedInUser"); // check login
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 }
+
 
