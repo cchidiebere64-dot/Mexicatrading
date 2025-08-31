545bca42c80457c17a757854b9ef647a5d067c7c
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-08-31 09:35:47 -0700

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,13 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  const user = localStorage.getItem("loggedInUser"); // check login
+  const user = sessionStorage.getItem("user");
 
+  // If no user in sessionStorage → redirect to login
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
+  // Else → allow access
   return children;
 }
