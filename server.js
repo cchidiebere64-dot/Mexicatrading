24b61f4c61a1159184a2a304278aecd3a96f0c54
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-08-31 10:40:38 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect DB
+connectDB();
+
+app.get("/", (req, res) => res.send("API Running 🚀"));
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
