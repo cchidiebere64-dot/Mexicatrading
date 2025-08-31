40f4ef51f66af14a77a762007c6b9c0eaabe807e
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-08-31 11:34:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -25,9 +25,12 @@ app.get("/", (req, res) => res.send("API Running 🚀"));
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/plans", planRoutes);         // ✅ plans API
+app.use("/api/dashboard", dashboardRoutes);
+
 app.use("/api/investments", investmentRoutes); // ✅ investments API
 
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
 
