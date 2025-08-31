e00f1e46181c73f59fa0f4ddf72a8e0ee484342e
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-08-31 11:26:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 // Routes
 import authRoutes from "./routes/authRoutes.js";
@@ -29,3 +30,4 @@ app.use("/api/investments", investmentRoutes); // ✅ investments API
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
