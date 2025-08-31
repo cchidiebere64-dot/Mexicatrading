2f23d9c57af2bd4a20dc8ce082f0d5f692383215
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-08-30 22:56:07 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
+import "./style.css";  // if you have Tailwind or custom styles
 
-import "./style.css";
-
-ReactDOM.createRoot(document.getElementById("app")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
