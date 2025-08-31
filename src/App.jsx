7e8a20d8f33fdd38beabee920c74767c85653cfb
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-08-31 08:58:41 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import Home from "./pages/Home";
 import Plans from "./pages/Plans";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/plans" element={<Plans />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/plans" element={<Plans />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
+  <Route path="/dashboard" element={<Dashboard />} />
+</Routes>
       <Footer />
     </Router>
   );
 }
+
