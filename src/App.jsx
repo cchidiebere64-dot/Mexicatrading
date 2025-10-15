710c5d168e9a843cca420836fdc182002d56c74c
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-10-15 13:08:37 -0700

    Refactor App component to render pages directly
    Removed routing and replaced with direct page rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Plans from "./pages/Plans";
@@ -7,8 +6,6 @@ import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Deposit from "./pages/Deposit";
 import Withdraw from "./pages/Withdraw";
-import ProtectedRoute from "./components/ProtectedRoute";
-import AdminRoute from "./components/AdminRoute";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminUsers from "./pages/AdminUsers";
 import AdminDeposits from "./pages/AdminDeposits";
@@ -17,94 +14,27 @@ import AdminPlans from "./pages/AdminPlans";
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        {/* Navbar always visible */}
-        <Navbar />
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar always visible */}
+      <Navbar />
 
-        {/* Main Content */}
-        <main className="flex-grow">
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/" element={<Home />} />
-            <Route path="/plans" element={<Plans />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-
-            {/* User Routes */}
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/deposit"
-              element={
-                <ProtectedRoute>
-                  <Deposit />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/withdraw"
-              element={
-                <ProtectedRoute>
-                  <Withdraw />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Admin Routes */}
-            <Route
-              path="/admin"
-              element={
-                <AdminRoute>
-                  <AdminDashboard />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/admin/users"
-              element={
-                <AdminRoute>
-                  <AdminUsers />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/admin/deposits"
-              element={
-                <AdminRoute>
-                  <AdminDeposits />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/admin/withdrawals"
-              element={
-                <AdminRoute>
-                  <AdminWithdrawals />
-                </AdminRoute>
-              }
-            />
-            <Route
-              path="/admin/plans"
-              element={
-                <AdminRoute>
-                  <AdminPlans />
-                </AdminRoute>
-              }
-            />
-          </Routes>
-        </main>
-
-        {/* Footer always visible */}
-        <Footer />
-      </div>
-    </Router>
+      {/* Main Content */}
+      <main className="flex-grow p-6">
+        {/* Render pages directly */}
+        <Dashboard />
+        {/* Uncomment other pages if needed */}
+        {/* <Home /> */}
+        {/* <Plans /> */}
+        {/* <Login /> */}
+        {/* <Register /> */}
+        {/* <Deposit /> */}
+        {/* <Withdraw /> */}
+        {/* <AdminDashboard /> */}
+        {/* <AdminUsers /> */}
+        {/* <AdminDeposits /> */}
+        {/* <AdminWithdrawals /> */}
+        {/* <AdminPlans /> */}
+      </main>
+    </div>
   );
 }
-
