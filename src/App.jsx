aa701ccfef1a8b2c8ccdcfb6f8c0e2289b32133e
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-10-15 13:10:52 -0700

    Simplify App component to only render Dashboard
    Removed unused imports and simplified the App component to only render the Dashboard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Plans from "./pages/Plans";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Deposit from "./pages/Deposit";
-import Withdraw from "./pages/Withdraw";
-import AdminDashboard from "./pages/AdminDashboard";
-import AdminUsers from "./pages/AdminUsers";
-import AdminDeposits from "./pages/AdminDeposits";
-import AdminWithdrawals from "./pages/AdminWithdrawals";
-import AdminPlans from "./pages/AdminPlans";
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Navbar always visible */}
-      <Navbar />
-
-      {/* Main Content */}
-      <main className="flex-grow p-6">
-        {/* Render pages directly */}
+      {/* Only render Dashboard */}
+      <main className="flex-grow">
         <Dashboard />
-        {/* Uncomment other pages if needed */}
-        {/* <Home /> */}
-        {/* <Plans /> */}
-        {/* <Login /> */}
-        {/* <Register /> */}
-        {/* <Deposit /> */}
-        {/* <Withdraw /> */}
-        {/* <AdminDashboard /> */}
-        {/* <AdminUsers /> */}
-        {/* <AdminDeposits /> */}
-        {/* <AdminWithdrawals /> */}
-        {/* <AdminPlans /> */}
       </main>
     </div>
   );
