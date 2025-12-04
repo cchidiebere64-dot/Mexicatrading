0966dc6c4c12dbb6b08cbd6a571a9f7ef954a0cb
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-12-04 06:09:19 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true, // auto opens browser
-  },
-  build: {
-    outDir: "dist",
-  },
 });
