b85bf477ed1d8811d0fd2a061454b17d49c1d68d
cchidiebere64-dot <cchidiebere64@gmail.com>
2026-05-04 19:18:47 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./style.css";
 import "./i18n.js";
 
-ReactDOM.createRoot(document.getElementById("admin-root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
